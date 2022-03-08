5aa9a68ebf72b0c61b88313e5d411cf21ba0af84
NathalyCristinaIFTM <nathaly.carvalho@estudante.iftm.edu.br>
2022-03-08 11:59:37 -0300

    Meu Primeiro Programa

---
+++ MeuPrimeiroPrograma.java
@@ -0,0 +1,6 @@
+public class MeuPrimeiroPrograma {
+public static void main(string[] args){
+System.out.println("Laboratório de Programação 2 - LP2");
+System.out.println("2 ano de informtica");
+System.out.println("Seja bem vindo!);
+System.out.println("Vamos aprender java.);
